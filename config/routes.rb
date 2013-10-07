baec1bff24219c0ec51a8511e58cb8b95ab9d9b9
Stephen <stephen@gmail.com>
2013-10-07 16:44:23 +0100

    test

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ FirstApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'Application#index'
+   root 'application#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
